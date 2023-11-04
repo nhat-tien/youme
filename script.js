acd179186070377707c619955d316c7b269e9d39
Nhat Tien <ledangnhattien04@gmail.com>
2023-11-04 19:05:14 +0700

    swiper

--- script.js
+++ script.js
@@ -3,4 +3,16 @@ const menu = document.getElementById("menu")
 
 toggleMenuBtn.addEventListener("click", () => {
     menu.classList.toggle("opened")
-})
+});
+
+var swiper1 = new Swiper(".swiper1", {
+    pagination: {
+        el: '.swiper-pagination1',
+        type: 'bullets',
+    },
+    paginationClickable: true,
+    autoplay: {
+        delay: 3000,
+    },
+    loop: true,
+});
